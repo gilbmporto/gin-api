dc6bb5c3a9313f93714eb090bf64146cc99406d8
gilbmporto <gilbmp@gmail.com>
2025-03-04 12:21:40 -0300

    Started Go project with the purpose of creating an API using the GIN framework

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Task struct {
+	Id    int    `json:"id"`
+	Title string `json:"title"`
+}
+
+func main() {
+	router := gin.Default()
+
+	router.SetTrustedProxies(nil)
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
+	})
+
+	router.Run(":3000")
+}
