de1d2ab54010acc925a95740309617370cf7ec1f
gilbmporto <gilbmp@gmail.com>
2025-03-11 15:03:02 -0300

    Add PATCH route to the API

--- main.go
+++ main.go
@@ -65,6 +65,35 @@ func main() {
 		ctx.JSON(http.StatusCreated, *newTask)
 	})
 
+	router.PATCH("/tasks/:id", func(ctx *gin.Context) {
+		id := ctx.Param("id")
+
+		// Convert the string ID to an integer
+		idInt, err := strconv.Atoi(id)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+			return
+		}
+
+		var updatedTask *Task
+		if err := ctx.ShouldBindJSON(&updatedTask); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Find the task with the given ID and update it
+		for index, task := range tasks {
+			if task.Id == (idInt) {
+				tasks[index].Title = updatedTask.Title
+				ctx.JSON(http.StatusOK, tasks[index])
+				return
+			}
+		}
+
+		// If the task is not found, return a 404 error
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+	})
+
 	router.DELETE("/tasks/:id", func(ctx *gin.Context) {
 		id := ctx.Param("id")
 
